6498a25dbd869e2f5dba2a309e5db37c96828d69
Shivacharitha <rithumateti@gmail.com>
2024-12-21 10:35:08 +0530

    adding 3 files together

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Apple from './Apple';
+import Ornage from './Orange';
+
+function App() {
+  return (
+    <div className="App">
+      <Apple name="shiva"/>
+      <Ornage/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
